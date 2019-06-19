56f4a072dcb966f91716073b1af9301749698fc7
granat-elna <bambooflow0522@gmail.com>
2019-06-19 17:50:09 +0900

    latest

--- det_main.py
+++ det_main.py
@@ -1,24 +1,46 @@
 # coding: utf-8
+# n次正方行列を求める実行用プログラム
 import numpy as np
 import numpy.linalg as LA
+import time
 import det_n
 
-mode = input("type '1' for randomly generated, or '2' for your specified >>> ")
-if mode == 1:
-    nth = 8
-elif mode == 2:
-    nth = input("type determinant size >>> ")
-x = np.empty((0, nth))
-x_row = np.empty(0)
-for i in range(nth):
-    for j in range(nth):
+# 行列のサイズを指定
+n = input("type determinant size n >>> ")
+# 行列の生成モードの選択. 1を入力するとランダム生成、2を入力するとマニュアル生成
+while True:
+    mode = input(
+        "type '1' to generate randomly, or '2' to specify manually >>> ")
+    if mode == 1 or mode == 2:
+        break
+    print("type just 1 or 2.")
+
+x = np.empty((0, n))  # (n)次正方行列を生成, すべての要素を0に初期化
+
+for i in range(n):
+    x_row = np.empty(0)  # (n)次正方行列の行部分を同様に0に初期化
+    for j in range(n):
+        # ランダム生成モードの場合、0.0から10.0の範囲の値を生成
         if mode == 1:
-            x_in = int(np.random.rand() * 10)
+            x_in = int(np.random.rand() * 100) * 0.1
+        # マニュアル生成モードの場合、各要素を手動で入力
         elif mode == 2:
             x_in = input("X[{}][{}] >>> ".format(i+1, j+1))
+        # 生成した値をx_rowの最後尾に追加
         x_row = np.append(x_row, x_in)
+    # 生成したx_rowを行列xに追加
     x = np.append(x, np.array([x_row]), axis=0)
-    x_row = np.empty(0)
+
 print("Requested determinant: \n{}".format(x))
-print("Determinant(numpy calculated): {}".format(LA.det(x)))
-print("Determinant(my program calculated): {}".format(det_n.detn(x, nth)))
+t1 = time.time()
+numpy_d = LA.det(x)
+t2 = time.time() - t1
+# NumPyの標準機能で求めた行列式(理論上の正解)
+print("Determinant(numpy calculated): {}\nexecution time: {} sec."
+      .format(numpy_d, t2))
+t1 = time.time()
+my_d = det_n.detn(x, n)
+t2 = time.time() - t1
+# detn関数で求めた行列式
+print("Determinant(my program calculated): {}\nexecution time: {} sec."
+      .format(my_d, t2))
--- det_n.py
+++ det_n.py
@@ -1,27 +1,32 @@
 # coding: utf-8
+# n次正方行列を求める本体のプログラム
 import numpy as np
 
 
-# NxN determinant
+# NxN determinant(完成版)
 def detn(x, n):
-    # 
-    d = 0
-    x_del = np.zeros((n, n))
+    d = 0  # 行列の値
+    x_del = np.zeros((n, n))  # 縮小した行列
+    # 行列のサイズが1
     if n == 1:
-        d = x[0][0]
-        return d
-    if n == 2:
-        d = x[0][0] * x[1][1] - x[0][1] * x[1][0]
-        return d
+        return x[0][0]
+    # 行列のサイズが2
+    elif n == 2:
+        return x[0][0] * x[1][1] - x[0][1] * x[1][0]
+    # 行列のサイズが3以上
     else:
         for i in range(0, n):
+            # 行列xの第1行を削除
             x_del = np.delete(x, 0, 0)
+            # 行列xの第i行を削除 これで元の行列のサイズnに対して(n-1)次正方行列ができた
             x_del = np.delete(x_del, i, 1)
+            # 縮小した行列を再帰してサイズを2以下にし、その値を結果dに足し合わせていく
+            # x[0][i]は余因子、pow(-1, i)は正負の値(i+jが奇数ならば(-1)を掛ける)
             d += x[0][i] * pow(-1, i) * detn(x_del, n-1)
         return d
 
 
-# 3x3 determinant
+# 3x3 determinant(テスト用)
 def det3(x):
     d = .0
     for i in range(3):
@@ -34,6 +39,6 @@ def det3(x):
     print("Determinant: {}".format(d))
 
 
-# 2x2 determinant
+# 2x2 determinant(テスト用)
 def det2(x):
     return x[0][0] * x[1][1] - x[0][1] * x[1][0]
